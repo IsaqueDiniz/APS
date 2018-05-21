724aa4df7160749ba957462fb8271ff3c9597569
IsaqueDiniz <37585530+IsaqueDiniz@users.noreply.github.com>
2018-05-21 12:18:04 -0300

    Mudanças Finais
    Site pronto

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,37 +1,51 @@
 /*Scripts*/
 
-// Rolagem suave com jQuery
-
-$(function() {
-    $('a').bind('click',function(event){ /*seleciona o elemento, atribui a função com a animação ao evento */
-        var $anchor = $(this);
-
-      $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'easeInOutExpo');
-    });
-});
-
-
 // Controladores do menu;
 
 let openMenuBtn = document.getElementById('openMenuBtn');
-	openMenuBtn.addEventListener("click", function(event){
-		document.getElementById('barraNav').style.background="#4286f4";
-		document.getElementById('barraNav').style.opacity="0.95";
-		document.getElementById('barraNav').style.width="98vw";
-		document.getElementById('barraNav').style.borderTop="10px #103B82 solid";
-		document.getElementById('ulMenu').style.visibility="initial";
-		closeMenuBtn.style.visibility="initial";
-	});
+	openMenuBtn.addEventListener("click", function(){
+	
+		if (document.getElementById('barraNav').style.width == "98vw") {
+			fechaMenu();
+		}else if(document.getElementById('barraNav').style.width == ""){
+			abreMenu();
+		}else if (document.getElementById('barraNav').style.width == "1%"){
+			abreMenu()
+		};
+	
+	}); 
+
+function abreMenu () {
+	document.getElementById('barraNav').style.background="#4286f4";
+	document.getElementById('barraNav').style.opacity="0.95";
+	document.getElementById('barraNav').style.width="98vw";
+	document.getElementById('barraNav').style.borderTop="10px #103B82 solid";
+	document.getElementById('ulMenu').style.visibility="initial";
+	closeMenuBtn.style.visibility="initial";	
+}
+
 
 let closeMenuBtn = document.getElementById('closeMenuBtn');
-	closeMenuBtn.addEventListener("click", function(event){
-		closeMenuBtn.style.visibility="hidden"
-		document.getElementById('ulMenu').style.visibility="hidden";
-		document.getElementById('barraNav').style.borderTop="initial";
-		document.getElementById('barraNav').style.width="1%";
-		document.getElementById('barraNav').style.background="transparent";
-	});	
+	closeMenuBtn.addEventListener("click", fechaMenu);	
 
+let linksAncoras = [document.getElementById('link0'), document.getElementById('link1'), document.getElementById('link2'), document.getElementById('link3'),];
+	
+	for(let i = 0; i < linksAncoras.length; i++){
+		linksAncoras[i].addEventListener("click", fechaMenu);
+	}	
+
+function fechaMenu() {
+	closeMenuBtn.style.visibility="hidden"
+	document.getElementById('ulMenu').style.visibility="hidden";
+	document.getElementById('barraNav').style.borderTop="initial";
+	document.getElementById('barraNav').style.width="1%";
+	document.getElementById('barraNav').style.background="transparent";	
+}
+
+// 
+
+
+// 
 // Controladores do Indice
 
 let fechaIndiceBTN = document.getElementById('fechaIndiceBTN');
@@ -47,7 +61,7 @@ let posicao = 0; /*posicao da ancora, cada ancora é um link no HTML*/
 
 
 let abreIndiceBTN = document.getElementById('abreIndiceBTN');
-	abreIndiceBTN.addEventListener("click", function(event){
+	abreIndiceBTN.addEventListener("click", function(){
 		abreIndiceBTN.style.visibility="hidden"
 		document.getElementById('indice').style.background="#FFF092"
 		document.getElementById('indice').style.width="auto";
@@ -67,9 +81,36 @@ function fechaIndice() {
 }
 
 
+//Configura botão para esconder e mostrar o texto;
+
+let ativador = document.getElementById('lerMaisBTN');
+let restoTexto = document.getElementById('restoDoTexto');
+	
+	ativador.addEventListener("click", function(){
+
+		if (restoTexto.style.fontSize == ""){
+			restoTexto.style.fontSize="1em";
+			ativador.innerHTML="Esconder";
+		}
+		else if (restoTexto.style.fontSize="1em"){
+			restoTexto.style.fontSize="";
+			ativador.innerHTML="Ler mais";
+		}
+	});
+
 // 
 // 
 // 
+
+// Rolagem suave com jQuery
+
+$(function() {
+    $('a').bind('click',function(event){ /*seleciona o elemento, atribui a função com a animação ao evento */
+        var $anchor = $(this);
+
+      $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top}, 1000,'easeInOutExpo');
+    });
+});
 	
 
 
